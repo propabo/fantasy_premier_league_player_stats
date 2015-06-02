c9e8fb66291b9b6b7b91636db549d027a548ced3
propabo <tom.godfrey@hotmail.co.uk>
2015-06-02 10:52:36 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -38,7 +38,7 @@ foreach($obj->fixture_history->all as $rows)
 
 //create results table
 $results = array(
-    'player_id' => strval($player),
+    #'player_id' => strval($player),
     'player_gw_index' => strval($player.$rows[1]),
     'first_name' => strval($obj->first_name),
     'last_name' => strval($obj->web_name),
